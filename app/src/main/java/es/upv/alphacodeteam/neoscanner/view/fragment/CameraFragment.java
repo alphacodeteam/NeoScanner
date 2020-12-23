0c58deb683f2e3c00519f423d18a3e31353d0394
1dmaol <marc97oller@gmail.com>
2020-12-23 01:21:50 +0100

    Pushing camera base.
    Needs to improve the layout and all related.

--- app/src/main/java/es/upv/alphacodeteam/neoscanner/view/fragment/CameraFragment.java
+++ app/src/main/java/es/upv/alphacodeteam/neoscanner/view/fragment/CameraFragment.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import es.upv.alphacodeteam.neoscanner.R;
+import es.upv.alphacodeteam.neoscanner.view.util.CameraManager;
 
-public class SecondFragment extends Fragment {
+public class CameraFragment extends Fragment {
+
+    CameraManager camera;
 
     @Override
     public View onCreateView(
@@ -25,12 +30,38 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        camera = new CameraManager(getContext());
+
+        FrameLayout preview = (FrameLayout) view.findViewById(R.id.camera_preview);
+        preview.addView(camera.getPreview());
+
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
+                NavHostFragment.findNavController(CameraFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
     }
+
+    /**
+     * Hide/Show the toolbar. Enable Fullscreen mode for camera preview.
+     **/
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+        camera.stop();
+    }
+
+
 }
